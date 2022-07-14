b67ae559190e1d0ebf2e14bc34a671d8513bdb89
fabrizioguespe <fabrizioguespe@MacFabri.local>
2022-07-14 17:11:36 -0300

    new

--- readme.md
+++ readme.md
@@ -3,12 +3,6 @@
 Hi! This is a discord bot i built using NodeJS for operating an Axie Infinity guild at scale.
 
 
-### Built With
-
-* [NodeJs](https://nodejs.org/en/)
-* [Web3js](https://web3js.readthedocs.io/)
-
-
 ## Getting Started
 
 I have put a mongo db for you to use it as a clean slate. I recommend using a mongoide so to import data from an excel or so. I use mingo.
@@ -26,23 +20,8 @@ You have to create a Discord Bot with enouth rights for the bot to operate.
    }
   ```
 
-
-### MongoDB
-
-MongoDB you will use it to track players names, wallets, payout addresses, etc.
-
-- Install MongoDB
-- Unzip db
-  ```
-  gunzip ronibot-db.gz -k    
-  ```
-- Import db
-  ```
-  mongorestore --archive=ronibot-db
-  ```
-
 ### Private keys
-For this to work you have to create a file containing all wallets and their private keys. Create a file secrets.json and copy into a structure like this one
+For this to work you have to create a file containing all wallets and their private keys. Create a file **secrets.json** and copy into a structure like this one
   ```
 {
     
@@ -53,18 +32,11 @@ For this to work you have to create a file containing all wallets and their priv
 }
   ```
 
-
-## Populate
-
-I have attached a sample of the users table. You can create a table of all your schollars and their addresses. Then relate it to others
-
-sample.xls
-
 ## Usage
 
 - Transfer axies from one wallet to another
 ```
-!transfer axie AXIE_ID,AXIE_ID FROM_ACCOUNT FROM_ACCOUNT
+!transfer axie AXIE_id1...,AXIE_idN FROM_ACCOUNT FROM_ACCOUNT
 ```
 
 - Transfer tokens or usd (converts usd to slp) from one wallet to another. If only one wallet specified the source will come from main wallet.
@@ -79,20 +51,15 @@ sample.xls
 !transfer usd QUANTITY FROM_ACCOUNT FROM_ACCOUNT
 ```
 
-- Update user field in Users DB table
-```
-!update ACCOUNT_NUMBER FIELD VALUE 
-```
-
 - Payment receipt for user
 ```
 !roni ACCOUNT
 ```
 
-- Reviews all wallets for missing or forgoten SLP and claims it or sends it back to main account
+- Flush account
 
 ```
-!cron flushall
+!flush ACCOUNT
 ```
 
 
---
+++ src/Commands/flush.js
@@ -0,0 +1,58 @@
+const Command = require("../Structures/Command.js");
+const fetch = require( "node-fetch")
+const { MessageEmbed} = require('discord.js');
+
+module.exports = new Command({
+	name: "help"+(process.env.LOGNAME=='fabrizioguespe'?'t':''),
+	async run(message, args, client) {
+		//Copiar desde aca
+		let db = await DbConnection.Get();
+		let users=await db.collection('users').find({nota:'retiro'}).toArray()
+		users=users.sort(function(a, b) {return parseInt(a.num) - parseInt(b.num)});
+		let slpp=0
+		
+		if(typeof message !== 'undefined' && message.channel)message.channel.send('Procesando los claims pendientes...')
+		for(let i in users){
+			let user=users[i]
+			if(!user.accountAddress || user.accountAddress.length!=46)continue
+			if(typeof args !== 'undefined' && args[2] && user.num!=args[2])continue
+			let data=await utils.getSLP(user.accountAddress,null,false)
+			user.in_game_slp=data.in_game_slp
+			console.log(user.num,data.in_game_slp)
+			if(data.in_game_slp>0){		
+				slpp+=data.in_game_slp
+				message.channel.send('#'+user.num+': Se encontraron '+user.in_game_slp+' SLP sin reclamar')
+				try{
+					await utils.claim(user,message)
+				}catch (e) {
+					utils.log(e,message)
+				}
+			}	
+		}
+		
+		if(typeof message !== 'undefined' && message.channel)utils.log(slpp +'SLP totales con una cantidad de registros: '+users.length,message);
+
+		slpp=0
+		if(typeof message !== 'undefined' && message.channel)message.channel.send('Revisando las wallets...')
+		for(let i in users){
+			let user=users[i]
+			if(!user.accountAddress || user.accountAddress.length!=46)continue
+			if(typeof args !== 'undefined' && args[2] && user.num!=args[2])continue
+			let data=await utils.getSLP(user.accountAddress,null,false)
+			console.log(user.num,data.ronin_slp)
+			user.ronin_slp=data.ronin_slp
+			if(data.ronin_slp>0){			
+				slpp+=data.ronin_slp
+				message.channel.send('#'+user.num+': Se encontraron '+user.ronin_slp+' SLP para transferir')
+				try{	
+					await utils.transfer(user.accountAddress,await utils.getWalletByNum("BREED"),user.ronin_slp,message)
+				}catch (e) {
+					utils.log(e,message)
+				}
+			}
+		}
+		
+		if(typeof message !== 'undefined' && message.channel)utils.log(slpp +'SLP totales con una cantidad de registros: '+users.length,message);
+		
+	}
+});
